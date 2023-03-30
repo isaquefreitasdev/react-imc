519ac59f827f577324400f294e0cb53985c158a7
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-03-30 11:12:58 -0300

    Validation with status of Imc

--- src/Calc.jsx
+++ src/Calc.jsx
@@ -5,8 +5,7 @@ function Calc() {
   const [weight, setWeight] = useState('');
   const [height, setHeight] = useState('');
   const [valueFinal, setValueFinal] = useState('');
-  const [writeImc,setWriteImc] = useState('');
- 
+  const [imcStatus, setImcStatus] = useState('');
 
   function handleValueWeight(event){
     const w = event.target.value;
@@ -30,29 +29,32 @@ function Calc() {
     if (!weight || !height) {
       // se algum campo estiver vazio, não calcula e exibe uma mensagem de erro
       setValueFinal('');
-    alert("Preecha os campos")
+      setImcStatus('');
+      alert("Preencha os campos");
       return;
     }
     const calc = eval(weight / (height * height));
-    setWeight('')
-    setHeight ('')
-    setValueFinal(Number(calc.toFixed(2)))
-    writeImcStatus();
-    
+    setWeight('');
+    setHeight('');
+    setValueFinal(Number(calc.toFixed(2)));
+    writeImcStatus(calc);
   }
-  function writeImcStatus(){
-    let condition = ""
-    if(valueFinal < 18){
-      condition = "Você está Abaixo do Peso"
-      setWriteImc(condition)
-      console.log(writeImc)
-    }else if(valueFinal >=18 && valueFinal <=24.5){
-      condition = "Você está no peso Ideal"
-      setWriteImc(condition)
 
-    }else if(valueFinal > 24.5){
-      condition = 'Procure um médico'
-      setWriteImc(condition)
+  function writeImcStatus(imcValue){
+    if (imcValue < 18.5) {
+      setImcStatus("Abaixo do peso");
+    } else if (imcValue >= 18.5 && imcValue <= 24.9) {
+      setImcStatus("Peso normal");
+    } else if (imcValue >= 25 && imcValue <= 29.9) {
+      setImcStatus("Sobrepeso");
+    } else if (imcValue >= 30 && imcValue <= 34.9) {
+      setImcStatus("Obesidade grau 1");
+    } else if (imcValue >= 35 && imcValue <= 39.9) {
+      setImcStatus("Obesidade grau 2");
+    } else if (imcValue >= 40) {
+      setImcStatus("Obesidade grau 3");
+    } else {
+      setImcStatus('');
     }
   }
 
@@ -64,16 +66,15 @@ function Calc() {
           <h1>Calculadora Imc</h1>
           <div className="inputsFields">
             <input value={weight} onChange={handleValueWeight} placeholder="Digite seu peso(kg)" id="field1" type="text" />
-            
             <input value={height} onChange={handleValueHeight} placeholder="Digite sua altura(m)" type="text" name="" id="field2" />
-
             <button onClick={ValueCalculator} id="calculatorButton">Calcular</button>
-            {valueFinal && <p>{valueFinal}</p>}
-            {writeImcStatus && <p>{writeImc}</p>}
+            {valueFinal && <p>IMC: {valueFinal}</p> }
+            {imcStatus && <p>Status: {imcStatus}</p>}
           </div>
         </div>
       </main>
     </>
   );
 }
+
 export default Calc;
